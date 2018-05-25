bf0429e18d8753d4d5cf019f9e69c8ed22b32ab3
elyrio <lyrio.nas@gmail.com>
2018-05-25 20:18:55 -0300

    Script para gerar tabela CMVCOL.

---
+++ Tabela_CMVCol_taco3_SQL.py
@@ -0,0 +1,31 @@
+#! /usr/bin/env/ python
+import string
+
+arquivo = open('taco_CMVCol.csv','r')
+arquivoSQL =open('tabelaCMVCol_taco3.sql','wr')
+conteudo=arquivo.readlines()
+
+tamanhoLista= len(conteudo)
+print 'tamanho da lista: ',tamanhoLista
+i=0
+for item in conteudo:
+    item=item.strip()
+    #print item
+
+listaSQL = []
+for item in conteudo:
+   item.replace("\n"," ")
+   item=item.strip()
+   linha=string.split(item,";")
+   dados="INSERT INTO CMVColtaco3 (id, categoria, descricaoAlimento, umidade, energiaKcal, energiaKj, proteina, lipideos, colesterol, carboidrato, fibraAlimentar, cinzas, calcio, magnesio, manganes, fosforo, ferro, sodio, potassio, cobre, zinco, retinol, re, rae, tiamina, riboflavina, piridoxina, niacina, vitaminaC, sdataGerado) VALUES (''"
+   t=0
+   while (t < len(linha)):
+       dados = dados+",'"+linha[t]+"'"
+       t=t+1
+   dados = dados+");\n"
+   arquivoSQL.writelines(dados)
+   i = i + 1
+   print 'feito a linha: ', i
+print '** FINALIZADO! Favor, verifique o aqruivo. **'
+arquivoSQL.close()
+arquivo.close()
