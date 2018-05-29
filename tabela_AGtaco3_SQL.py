b3525b459fc5ded96b8245f2d9bf38b6a244b02a
elyrio <lyrio.nas@gmail.com>
2018-05-28 22:02:21 -0300

    Criando script  gerador insert tabela AG.

--- tabela_AGtaco3_SQL.py
+++ tabela_AGtaco3_SQL.py
@@ -17,7 +17,7 @@ for item in conteudo:
    item.replace("\n"," ")
    item=item.strip()
    linha=string.split(item,";")
-   dados="INSERT INTO AGtaco3 (id, categoria, descricaoAlimento, saturados, monoinsaturados, poliinsaturados, 12_0, 14_0, 16_0, 18_0, 20_0, 22_0, 24_0, 14_1, 16_1, 18_1, 20_1, 18_2N_6, 18_3N_3, 20_4, 20_5, 22_5, 22_6, 18_1t, 18_2t, created_at, updated_at) VALUES (''"
+   dados="INSERT INTO AGtaco3 (id, categoria, numeroAlimento, descricaoAlimento, saturados, monoinsaturados, poliinsaturados, 12_0, 14_0, 16_0, 18_0, 20_0, 22_0, 24_0, 14_1, 16_1, 18_1, 20_1, 18_2N_6, 18_3N_3, 20_4, 20_5, 22_5, 22_6, 18_1t, 18_2t, created_at, updated_at) VALUES (''"
    t=0
    while (t < len(linha)):
        dados = dados+",'"+linha[t]+"'"
